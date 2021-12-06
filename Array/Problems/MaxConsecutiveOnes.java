365f1f629e4e8b66c0100e024e20cf3c63783073
nicolaDeCristofaro <nico.de.cristofaro97@gmail.com>
2021-12-06 21:48:55 +0100

    array max consecutive ones

---
+++ Array/Problems/MaxConsecutiveOnes.java
@@ -0,0 +1,17 @@
+package Array.Problems;
+
+//Given a binary array nums, return the maximum number of consecutive 1's in the array.
+class Solution {
+    public int findMaxConsecutiveOnes(int[] nums) {
+        int max = 0, count = 0;
+        
+        for (int i=0; i<nums.length; i++){
+            if (nums[i] == 1) count++;
+            if (nums[i] == 0 || i == nums.length-1 ){
+                if(count > max) max = count;
+                count = 0;
+            }
+        }
+        return max;
+    }
+}
